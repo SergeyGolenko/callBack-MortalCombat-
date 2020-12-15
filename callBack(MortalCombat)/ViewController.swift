6f5512d083bc4d326f4bd9d92e4c83fc6ae49443
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-15 11:18:42 +0200

    добавил кнопку и UIImage на первый контроллер

--- callBack(MortalCombat)/ViewController.swift
+++ callBack(MortalCombat)/ViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var imageMC: UIImageView!
+    @IBAction func selectPlayer(_ sender: UIButton) {
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
